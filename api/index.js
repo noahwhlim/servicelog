73dbb0e378f969ada3df7c046ad06de349770207
noahwhlim <noahhlimwh@gmail.com>
2024-07-19 19:53:48 -0500

    reinstalled packages in correct directory

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from "express"
-import mongoose from "mongoose"
 
-mongoose
-    .connect(process.env.MONGO)
+const app = express();
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000!!")
+})
